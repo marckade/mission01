f73de1da6460014d1b3e079a1a2d63ce505221cb
Kaden Marchetti <marckade@isu.edu>
2019-01-18 16:22:31 -0700

    Built node class and fixed singlylinkedlist class

---
+++ src/main/java/edu/isu/cs/cs3308/structures/Node.java
@@ -0,0 +1,24 @@
+package edu.isu.cs.cs3308.structures;
+
+public class Node<E>
+{
+    private Node<E> next;
+
+    private E data;
+
+    public Node<E> getNext() {
+        return next;
+    }
+
+    public void setNext(Node<E> next) {
+        this.next = next;
+    }
+
+    public E getData() {
+        return data;
+    }
+
+    public void setData(E data) {
+        this.data = data;
+    }
+}
--- src/main/java/edu/isu/cs/cs3308/structures/impl/SinglyLinkedList.java
+++ src/main/java/edu/isu/cs/cs3308/structures/impl/SinglyLinkedList.java
@@ -1,55 +1,53 @@
 package edu.isu.cs.cs3308.structures.impl;
 
 import edu.isu.cs.cs3308.structures.List;
+import edu.isu.cs.cs3308.structures.Node;
 
 public class SinglyLinkedList<E> implements List<E>
 {
 
 
+    protected Node<E> head;
 
-    private double score;
-    
+    protected Node<E> tail;
 
-    public double getScore() {
-        return score;
-    }
-
-    public void setScore(double score) {
-        this.score = score;
-    }
-
-
-
+    @Override
     public E first()
     {
-
+        return null;
     }
 
+    @Override
     public E last()
     {
-
+        return null;
     }
 
+    @Override
     public void addLast(E node)
     {
 
     }
 
+    @Override
     public void addFirst(E node)
     {
 
     }
 
+    @Override
     public E removeFirst()
     {
-
+        return null;
     }
 
+    @Override
     public E removeLast()
     {
-
+        return null;
     }
 
+    @Override
     public void insert(E node, int index)
     {
 
@@ -57,22 +55,25 @@ public class SinglyLinkedList<E> implements List<E>
 
     public E remove(int index)
     {
-
+        return null;
     }
 
+    @Override
     public E get(int index)
     {
-
+        return null;
     }
 
+    @Override
     public int size()
     {
-
+        return 0;
     }
 
+    @Override
     public boolean isEmpty()
     {
-
+        return false;
     }
 
     public void printList()
