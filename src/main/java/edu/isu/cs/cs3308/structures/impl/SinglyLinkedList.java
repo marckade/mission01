1fd86fc839f24fe18bf3cae44bb968613f7cde9f
Kaden Marchetti <marckade@isu.edu>
2019-01-18 15:37:41 -0700

    Built singlylist class and built most.

---
+++ src/main/java/edu/isu/cs/cs3308/structures/impl/SinglyLinkedList.java
@@ -0,0 +1,88 @@
+package edu.isu.cs.cs3308.structures.impl;
+
+import edu.isu.cs.cs3308.structures.List;
+
+public class SinglyLinkedList<E> implements List<E>
+{
+
+
+
+    private double score;
+    
+
+    public double getScore() {
+        return score;
+    }
+
+    public void setScore(double score) {
+        this.score = score;
+    }
+
+
+
+    public E first()
+    {
+
+    }
+
+    public E last()
+    {
+
+    }
+
+    public void addLast(E node)
+    {
+
+    }
+
+    public void addFirst(E node)
+    {
+
+    }
+
+    public E removeFirst()
+    {
+
+    }
+
+    public E removeLast()
+    {
+
+    }
+
+    public void insert(E node, int index)
+    {
+
+    }
+
+    public E remove(int index)
+    {
+
+    }
+
+    public E get(int index)
+    {
+
+    }
+
+    public int size()
+    {
+
+    }
+
+    public boolean isEmpty()
+    {
+
+    }
+
+    public void printList()
+    {
+
+    }
+
+
+
+
+
+
+}
